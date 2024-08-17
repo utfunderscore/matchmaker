2e8c4fb68f4cc7a24eb1350d3f50e18a211823da
Connor <concon477@virginmedia.com>
2024-08-17 14:27:33 +0100

    fix: Remove expressible in favor of internal result type

--- Shared/build.gradle.kts
+++ Shared/build.gradle.kts
@@ -10,9 +10,6 @@ version = "1.0-SNAPSHOT"
 dependencies {
     // add fastjson2
     implementation("com.alibaba:fastjson:2.0.51")
-
-    api("org.panda-lang:expressible:1.3.6") // Core library
-    api("org.panda-lang:expressible-kt:1.3.6") // Kotlin extensions
 }
 
 publishing {
---
+++ Shared/src/main/kotlin/org/readutf/matchmaker/shared/result/Result.kt
@@ -0,0 +1,24 @@
+package org.readutf.matchmaker.shared.result
+
+class Result<T>(
+    private val value: T?,
+    private val error: String?,
+) {
+    fun isOk(): Boolean = error == null
+
+    fun isError(): Boolean = error != null
+
+    fun <U> map(f: (T) -> U): Result<U> = if (isOk()) ok(f(value!!)) else error(error!!)
+
+    fun <U> flatMap(f: (T) -> Result<U>): Result<U> = if (isOk()) f(value!!) else error(error!!)
+
+    fun get(): T = value!!
+
+    fun getError(): String = error!!
+
+    companion object {
+        fun <T> error(error: String): Result<T> = Result(null, error)
+
+        fun <T> ok(value: T): Result<T> = Result(value, null)
+    }
+}
